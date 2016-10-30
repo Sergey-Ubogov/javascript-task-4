d89be7b03f118fd04cb94b95aac71c64f5756fdf
Sergey Ubogov <sergeiubogov@mail.ru>
2016-10-30 22:00:33 +0500

    дратути

--- lego.js
+++ lego.js
@@ -1,10 +1,6 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализованы методы or и and
- */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Запрос к коллекции
@@ -13,58 +9,128 @@ exports.isStar = true;
  * @returns {Array}
  */
 exports.query = function (collection) {
-    return collection;
+    var copyCollection = collection.slice();
+    console.info(copyCollection);
+    var functions = [];
+    for (var i = 1; i < arguments.length; i++) {
+        functions.push(arguments[i]);
+    }
+    var priorityFunctions = ['filterIn', 'sortBy', 'select', 'format', 'limit'];
+    functions.sort(function (functionOne, functionTwo) {
+        return priorityFunctions.indexOf(functionOne.name) -
+        priorityFunctions.indexOf(functionTwo.name);
+    });
+    //  console.info(functions);
+    functions.forEach(function (func) {
+        copyCollection = func(copyCollection);
+        console.info(copyCollection);
+    });
+
+    return copyCollection;
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Array}
  */
 exports.select = function () {
-    return;
+    var selectFields = [];
+    for (var i = 0; i < arguments.length; i++) {
+        selectFields.push(arguments[i]);
+    }
+
+    return function select() {
+        return arguments[0].map(function (friend) {
+            var requiredFields = {};
+            selectFields.forEach(function (field) {
+                requiredFields[field] = friend[field];
+            });
+
+            return requiredFields;
+        });
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Array}
  */
 exports.filterIn = function (property, values) {
     console.info(property, values);
 
-    return;
+    return function filterIn() {
+        var filteredList = [];
+        arguments[0].forEach(function (friend) {
+            if (!friend.hasOwnProperty(property)) {
+                return;
+            }
+            values.forEach(function (value) {
+                if (friend[property] === value) {
+                    filteredList.push(friend);
+                }
+            });
+        });
+
+        return filteredList;
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Array}
  */
 exports.sortBy = function (property, order) {
     console.info(property, order);
 
-    return;
+    return function sortBy() {
+        return arguments[0].sort(function (friendOne, friendTwo) {
+            var ord = order === 'asc' ? 1 : -1;
+            if (friendOne[property] > friendTwo[property]) {
+                return ord;
+            }
+            if (friendOne[property] < friendTwo[property]) {
+                return -ord;
+            }
+
+            return 0;
+        });
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Array}
  */
 exports.format = function (property, formatter) {
     console.info(property, formatter);
 
-    return;
+    return function format() {
+        return arguments[0].map(function (friend) {
+            friend[property] = formatter(friend[property]);
+
+            return friend;
+        });
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Array}
  */
 exports.limit = function (count) {
     console.info(count);
 
-    return;
+    return function limit() {
+        return arguments[0].slice(0, count);
+    };
 };
 
 if (exports.isStar) {
